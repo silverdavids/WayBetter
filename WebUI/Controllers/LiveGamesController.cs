d527be8ffb7d5981a31545715e77c22ef6c80f9a
Paul Mugeere <paul@laboremus.no>
2014-10-13 15:26:24 +0300

    Adding LiveGames

---
+++ WebUI/Controllers/LiveGamesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class LiveGamesController : Controller
+    {
+        //
+        // GET: /LiveGames/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
