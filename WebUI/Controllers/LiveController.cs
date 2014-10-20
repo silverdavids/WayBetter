22cfcb92a3419799ca037810edb79fb9cd7b14bc
Paul Mugeere <paul@laboremus.no>
2014-10-20 20:57:26 +0300

    Adding remo

---
+++ WebUI/Controllers/LiveController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class LiveController : Controller
+    {
+        //
+        // GET: /LiveGames/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Games()
+        {
+            return View();
+        }
+
+        public ActionResult RemoveThis()
+        {
+            return null;
+        }
+	}
+}
